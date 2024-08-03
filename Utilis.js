b11a8e2de5e2a5f613e26ec0c70d6eb9955ce3c1
AranDevX <aranarasmus@gmail.com>
2024-08-03 18:14:03 +0330

    made a function that Creates List and Task together also saves the data in a data.json file and creates it if it doesn't exist using fs module

--- Utilis.js
+++ Utilis.js
@@ -1,1 +1,45 @@
 const fs = require ("fs");
+
+const createListTask = (List, Task) => {
+    try {
+                // check if the json file exists
+    fs.access('data.json', (err) => {
+        // if it does not exist, create a new json file
+        if (err){
+            fs.writeFileSync('data.json', JSON.stringify([]))
+        }
+         // read from the todo.json if it exists
+        const todoBuffer = fs.readFileSync("data.json");
+        // convert it to string
+        let dataJSON = todoBuffer.toString();
+        // parse the data
+        const todos = JSON.parse(dataJSON);
+
+        // check if the todo List exists
+        const duplicateTodo = todos.find((task) => {
+            return task.list === list;
+        })
+
+        if (!duplicateTodo) {
+            todos.push({
+                List: List,
+                Task: Task,
+            });
+            dataJSON = JSON.stringify(todos);
+            fs.writeFileSync("data.json", dataJSON);
+            console.log("New Todo List Added");
+        } else {
+            console.log("New Todo List has already been used");
+        }
+
+    })
+
+    }catch(error){
+        console.log("An error occured, try again")
+    }
+    
+};
+
+module.exports = {
+    createListTask,
+};
