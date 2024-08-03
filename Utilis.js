abf5848eeb36ae86f31778242e55fc355559b697
AranDevX <aranarasmus@gmail.com>
2024-08-03 18:51:00 +0330

    made a function that Creates List and Task together also saves the data in a data.json file and creates it if it doesn't exist using fs module

--- Utilis.js
+++ Utilis.js
@@ -1,43 +1,36 @@
-const fs = require ("fs");
+const fs = require("fs");
 
-const createListTask = (List, Task) => {
+const createListTask = (list, task) => {
     try {
-                // check if the json file exists
-    fs.access('data.json', (err) => {
-        // if it does not exist, create a new json file
-        if (err){
-            fs.writeFileSync('data.json', JSON.stringify([]))
+        // check if the json file exists
+        try {
+            fs.accessSync('datas.json');
+        } catch (err) {
+            // if it does not exist, create a new json file
+            fs.writeFileSync('datas.json', JSON.stringify([]));
         }
-         // read from the todo.json if it exists
-        const todoBuffer = fs.readFileSync("data.json");
+
+        // read from the todo.json if it exists
+        const todoBuffer = fs.readFileSync("datas.json");
         // convert it to string
         let dataJSON = todoBuffer.toString();
         // parse the data
         const todos = JSON.parse(dataJSON);
 
         // check if the todo List exists
-        const duplicateTodo = todos.find((task) => {
-            return task.list === list;
-        })
+        const duplicateTodo = todos.find((task) => task.list === list);
 
         if (!duplicateTodo) {
-            todos.push({
-                List: List,
-                Task: Task,
-            });
+            todos.push({ list: list, task: task });
             dataJSON = JSON.stringify(todos);
-            fs.writeFileSync("data.json", dataJSON);
+            fs.writeFileSync("datas.json", dataJSON);
             console.log("New Todo List Added");
         } else {
-            console.log("New Todo List has already been used");
+            console.log("Todo List already exists");
         }
-
-    })
-
-    }catch(error){
-        console.log("An error occured, try again")
+    } catch (error) {
+        console.log("An error occurred, try again");
     }
-    
 };
 
 module.exports = {
--- index.js
+++
@@ -1,5 +0,0 @@
-// Master
-const yargs = require("yargs");
-const Utilis = require("./Utilis");
-
-//this is where we will put create List first
