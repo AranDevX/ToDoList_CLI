48eb35058a55353c21bba823636586922ac38bb3
AranDevX <aranarasmus@gmail.com>
2024-08-03 19:14:15 +0330

    updated the utilis.js file to see if it can handle multiple tasks for one list

--- Utilis.js
+++ Utilis.js
@@ -2,32 +2,43 @@ const fs = require("fs");
 
 const createListTask = (list, task) => {
     try {
-        // check if the json file exists
+        // Check if the JSON file exists
         try {
             fs.accessSync('datas.json');
         } catch (err) {
-            // if it does not exist, create a new json file
+            // If it does not exist, create a new JSON file
             fs.writeFileSync('datas.json', JSON.stringify([]));
         }
 
-        // read from the todo.json if it exists
+        // Read from the JSON file if it exists
         const todoBuffer = fs.readFileSync("datas.json");
-        // convert it to string
+        // Convert it to string
         let dataJSON = todoBuffer.toString();
-        // parse the data
+        // Parse the data
         const todos = JSON.parse(dataJSON);
 
-        // check if the todo List exists
-        const duplicateTodo = todos.find((task) => task.list === list);
-
-        if (!duplicateTodo) {
-            todos.push({ list: list, task: task });
-            dataJSON = JSON.stringify(todos);
-            fs.writeFileSync("datas.json", dataJSON);
-            console.log("New Todo List Added");
-        } else {
-            console.log("Todo List already exists");
+        // Find the todo list
+        let listExists = false;
+        for (let i = 0; i < todos.length; i++) {
+            if (todos[i].list === list) {
+                listExists = true;
+                todos[i].tasks.push(task);
+                break;
+            }
         }
+
+        // If the list doesn't exist, create a new one
+        if (!listExists) {
+            todos.push({
+                list: list,
+                tasks: [task],
+            });
+        }
+
+        // Write back to the JSON file
+        dataJSON = JSON.stringify(todos, null, 2);
+        fs.writeFileSync("datas.json", dataJSON);
+        console.log("Task added successfully");
     } catch (error) {
         console.log("An error occurred, try again");
     }
