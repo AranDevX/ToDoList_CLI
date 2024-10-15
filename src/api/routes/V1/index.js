706c6e811888b66120d83a3e00cf6dcabff60861
AranDevX <aranarasmus@gmail.com>
2024-10-15 23:10:35 +0330

    added new codes for task path and js files in index.js

--- src/api/routes/V1/index.js
+++ src/api/routes/V1/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const listRoutes = require('./list/listRout');  // List routes
 const userRoutes = require('./user/authRout');  // User routes
+const taskRoutes = require('./task/taskRout');  // Task routes
 
 const router = express.Router();
 
 // Mount list routes at /lists
 router.use('/lists', listRoutes);
+
+// Mount task routes at /tasks
+router.use('/tasks', taskRoutes);  
 
 // Mount user routes at /user
 router.use('/user', userRoutes);
